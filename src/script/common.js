ed4d1020bbf865fdb4342a2bddb528624a19288d
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-07-12 11:09:13 +0530

    update scroll animation

--- src/script/common.js
+++ src/script/common.js
@@ -61,3 +61,17 @@ function TypeWriter() {
   setTimeout(TypeWriter, speed);
 }
 TypeWriter();
+
+// scroll effect
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("opacity-0", "translate-y-20");
+      entry.target.classList.add("opacity-100", "translate-y-0");
+    }
+  });
+});
+document.querySelectorAll(".scroll-reval").forEach((el) => {
+  observer.observe(el);
+});
