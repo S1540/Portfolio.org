50a38d9e79cc01b0b507ddf9a4243af014e6b34e
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-07-14 02:14:50 +0530

    skills section update

--- src/script/script.js
+++ src/script/script.js
@@ -29,8 +29,18 @@ window.addEventListener("load", () => {
     loader.classList.add("hidden");
   }, 800);
 });
-
-// Bar btn for mobile menu toggle
+// scrolling animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("opacity-0", "translate-y-20");
+      entry.target.classList.add("opacity-100", "translate-y-0");
+    }
+  });
+});
+document.querySelectorAll(".scroll-reval").forEach((el) => {
+  observer.observe(el);
+});
 
 //Typewriter effect..................
 
@@ -66,35 +76,78 @@ function TypeWriter() {
   setTimeout(TypeWriter, speed);
 }
 TypeWriter();
-
-//  Skill Cards...........................
-// const Cards = [
-//   {
-//     title : 'HTML',
-//     description : 'Hypertext Markup Language Backbone Of Web Development:',
-//     icon : 'fa-brands fa-html5',
-//     color : 'bg-orange-500',
-//     textColor : 'text-white',
-//   },
-//   {
-//     title : 'CSS',
-//     description : 'Cascading Style Sheets For Styling Web Pages:',
-//     icon : 'fa-brands fa-css3-alt',
-//     color : 'bg-blue-500',
-//     textColor : 'text-white',
-//   },
-//   {
-//     title : 'JavaScript',
-//     description : 'Dynamic Programming Language For Web Development:',
-//     icon : 'fa-brands fa-js',
-//     color : 'bg-red-500',
-//     textColor : 'text-white',
-//   },
-//   {
-//     title : 'React',
-//     description : 'JavaScript Library For Building User Interfaces:',
-//     icon : 'fa-brands fa-react',
-//     color : 'bg-cyan-400',
-//     textColor : 'text-white'
-//   }
-// ]
+// skilss part or animation
+const skillsItem = [
+  {
+    name: "HTML",
+    progress: "96%",
+    icon: "./assets/html.png",
+    color: "bg-red-500",
+    colorProgress: "w-[96%]",
+  },
+  {
+    name: "CSS",
+    progress: "90%",
+    icon: "./assets/css-alt.png",
+    color: "bg-blue-500",
+    colorProgress: "w-[90%]",
+  },
+  {
+    name: "JavaScript",
+    progress: "85%",
+    icon: "./assets/javascript.png",
+    color: "bg-yellow-500",
+    colorProgress: "w-[85%]",
+  },
+  {
+    name: "React",
+    progress: "85%",
+    icon: "./assets/react.png",
+    color: "bg-cyan-400",
+    colorProgress: "w-[85%]",
+  },
+  {
+    name: "Tailwind CSS",
+    progress: "95%",
+    icon: "./assets/tailwind-css.png",
+    color: "bg-cyan-500",
+    colorProgress: "w-[95%]",
+  },
+  {
+    name: "Bootstrap",
+    progress: "70%",
+    icon: "./assets/bootstrap.png",
+    color: "bg-purple-500",
+    colorProgress: "w-[70%]",
+  },
+];
+const container = document.querySelector("#skillDiv");
+skillsItem.forEach((skill) => {
+  container.innerHTML += `<div class="max-w-lg w-full flex justify-around text-white">
+          <img src="${skill.icon}" class="w-20 h-20" />
+          <div class="max-w-sm w-full flex flex-col justify-around">
+            <div class="w-full flex justify-between">
+              <h1>${skill.name}</h1>
+              <h1>${skill.progress}</h1>
+            </div>
+            <div
+              class="relative h-2 w-full overflow-hidden bg-gray-300 rounded-md"
+            >
+              <span
+                class="html-bar h-2 w-0 rounded-md  transition-all duration-2000 ease-in-out block"
+              ></span>
+            </div>
+          </div>
+        </div>`;
+});
+window.addEventListener("scroll", () => {
+  const section = document.getElementById("skills-bar").getBoundingClientRect();
+  if (section.top < window.innerHeight) {
+    const htmlBars = document.querySelectorAll(".html-bar");
+    htmlBars.forEach((bar, index) => {
+      bar.classList.remove("w-0");
+      bar.classList.add(skillsItem[index].colorProgress);
+      bar.classList.add(skillsItem[index].color);
+    });
+  }
+});
