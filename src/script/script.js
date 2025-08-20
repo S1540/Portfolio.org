179651b37f86d99c2464ff86a6469e387a93fd15
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-08-20 18:23:49 +0530

    add map section

--- src/script/script.js
+++ src/script/script.js
@@ -35,6 +35,9 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       entry.target.classList.remove("opacity-0", "translate-y-20");
       entry.target.classList.add("opacity-100", "translate-y-0");
+    } else {
+      entry.target.classList.remove("opacity-100", "translate-y-0");
+      entry.target.classList.add("opacity-0", "translate-y-20");
     }
   });
 });
